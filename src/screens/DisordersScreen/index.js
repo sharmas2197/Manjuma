c20002fed1844e704c3adc0b0b2678c32d76407d
Shagun-LT <shagun.sharma@limetray.com>
2024-11-27 16:05:34 +0530

    modified disorders screen

--- src/screens/DisordersScreen/index.js
+++ src/screens/DisordersScreen/index.js
@@ -1,72 +1,70 @@
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import {View, Text, TouchableWithoutFeedback} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import LottieView from 'lottie-react-native';
-import styles from './styles';
-import Colors from '../../CustomeStyles/Colors';
 import LinearGradient from 'react-native-linear-gradient';
+import styles from './styles';
 import {quiz1} from '../../data/test1data';
 import {quiz2} from '../../data/test2data';
 import {quiz3} from '../../data/test3data';
 
-const DisordersScreen = props => {
-  const navigation = props.navigation;
+const DisordersScreen = ({navigation}) => {
   const testData = [
     {
-      name: 'GLOBAL DEVELOPMENTAL DELAY',
-      color: Colors.yellow,
-      lottie: require('../../res/quiz2.json'),
+      name: 'Global Developmental Delay',
+      color: '#FEF08A',
+      secondaryColor: '#FDE047',
+      lottie: require('../../res/brain2.json'),
       disorderId: 1,
       data: quiz1,
+      textColor: '#6B7280',
     },
     {
-      name: 'AUTISM SPECTRUM DISORDER',
-      color: Colors.lightPink,
-      lottie: require('../../res/art1.json'),
+      name: 'Autism Spectrum Disorder',
+      color: '#FDA4AF',
+      secondaryColor: '#FB7185',
+      lottie: require('../../res/brain3.json'),
       disorderId: 2,
       data: quiz2,
+      textColor: '#7F1D1D',
     },
     {
-      name: 'ATTENTION DEFICIT/HYPERACTIVITY DISORDER',
-      color: Colors.lightBlue,
-      lottie: require('../../res/food1.json'),
+      name: 'Attention Deficit/Hyperactivity Disorder',
+      color: '#93C5FD',
+      secondaryColor: '#60A5FA',
+      lottie: require('../../res/brain.json'),
       disorderId: 3,
       data: quiz3,
+      textColor: '#1E3A8A',
     },
   ];
 
-  const navigate = (id, name, data) => {
-    navigation.navigate('TestListScreen', {
-      disorderId: id,
-      disorderName: name,
-      data: data,
-    });
-  };
-
-  function ItemBox(test, index) {
-    let lottiePath = test.lottie;
-    return (
-      <TouchableWithoutFeedback
-        key={index}
-        onPress={() => navigate(test.disorderId, test.name, test.data)}>
-        <View
-          style={[
-            styles.boxStyle,
-            {
-              backgroundColor: test.color,
-            },
-          ]}>
-          <LottieView
-            source={lottiePath}
-            style={{height: 100, width: 100}}
-            autoPlay
-            loop
-          />
-          <Text style={styles.testName}>{test.name}</Text>
-        </View>
-      </TouchableWithoutFeedback>
-    );
-  }
+  const renderDisorderCard = (test, index) => (
+    <TouchableOpacity
+      key={index}
+      activeOpacity={1}
+      onPress={() =>
+        navigation.navigate('TestListScreen', {
+          disorderId: test.disorderId,
+          disorderName: test.name,
+          data: test.data,
+        })
+      }>
+      <LinearGradient
+        colors={[test.color, test.secondaryColor]}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+        style={styles.boxStyle}>
+        <LottieView
+          source={test.lottie}
+          style={{height: 80, width: 80}}
+          autoPlay
+          loop
+        />
+        <Text style={[styles.testName, {color: test.textColor}]}>{test.name}</Text>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
 
   return (
     <LinearGradient
@@ -75,9 +73,7 @@ const DisordersScreen = props => {
       end={{x: 1, y: 1}}
       style={styles.container}>
       <Text style={styles.headingText}>Disorders</Text>
-      {testData.map((item, index) => {
-        return ItemBox(item, index);
-      })}
+      {testData.map(renderDisorderCard)}
     </LinearGradient>
   );
 };
